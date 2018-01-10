a3b89278da57c7ed520cdaffdad76a01e3805703
Kosticeva <kosticka.jelena@gmail.com>
2018-01-10 19:04:18 +0100

    All digits on video recognized.

--- video_digit_detector.py
+++ video_digit_detector.py
@@ -3,42 +3,44 @@ import video_utils
 import image_utils
 
 
-video = cv2.VideoCapture('test samples/demo.avi')
+for i in range(0,10):
+    video = cv2.VideoCapture('test samples/video-'+str(i)+'.avi')
 
-if video.isOpened() == 0:
-    print("Error!!!")
+    if video.isOpened() == 0:
+        print("Error!!!")
 
-v_w = int(video.get(3))
-v_h = int(video.get(4))
+    v_w = int(video.get(3))
+    v_h = int(video.get(4))
 
-print(v_w)
-print(v_h)
+    print(v_w)
+    print(v_h)
 
-new_video = cv2.VideoWriter('test samples/demo_gr.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 40, (v_w, v_h))
+    new_video = cv2.VideoWriter('test samples/video_gr-'+str(i)+'.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 40, (v_w, v_h))
 
-idx = 0
-while video.isOpened():
+    idx = 0
+    while video.isOpened():
 
-    ret, frame = video.read()
-    if ret:
+        ret, frame = video.read()
+        if ret:
 
-        img = image_utils.image_bin(image_utils.image_gray(frame))
-        img_bin = image_utils.erode(image_utils.dilate(img))
+            img = image_utils.image_bin(image_utils.image_gray(frame))
+            img_bin = image_utils.erode(image_utils.dilate(img))
 
-        selected_regions, numbers = image_utils.select_roi(frame.copy(), img_bin)
-        cv2.imwrite("contoured_frames/frame_" + str(idx) + ".jpg", selected_regions)
+            selected_regions, numbers = video_utils.select_roi(frame.copy(), img_bin, idx, i)
+            cv2.imwrite("contoured_frames/video_"+str(i)+"/frame_" + str(idx) + ".jpg", selected_regions)
 
-        new_video.write(selected_regions)
-        cv2.imshow("Video", selected_regions)
+            new_video.write(selected_regions)
+            cv2.imshow("Video"+str(i), selected_regions)
 
-        idx = idx + 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            idx = idx + 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        else:
             break
 
-    else:
-        break
+    video.release()
+    new_video.release()
 
-video.release()
-new_video.release()
 
 cv2.destroyAllWindows()
--- video_utils.py
+++ video_utils.py
@@ -2,26 +2,25 @@ import cv2
 import image_utils
 
 
-def select_roi(image_orig, image_binn):
+def select_roi(image_orig, image_binn, idz, video_num):
 
-    cv2.imwrite("C:/Users/Jelena/Desktop/regions_preRect.png", image_binn)
     img, contours, hierarchy = cv2.findContours(image_binn.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     regions_array = []
 
-    filee = open("C:/Users/Jelena/Desktop/regions.txt", "a")
-    filee.write("INDEX\tX_COORD\tY_COORD\tWIDTH\tHEIGHT\tAREA\tFRAME")
+    filee = open("results/regions"+str(video_num)+".txt", "a")
+    filee.write("\n\nINDEX\tX_COORD\tY_COORD\tWIDTH\tHEIGHT\tAREA\tFRAME")
 
     idx = 0
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)  # koordinate i velicina granicnog pravougaonika
         area = cv2.contourArea(contour)
 
-        if area > 10 and area < 250 and ((h>7 and w<5) or (h>5 and w>5 and area>15)):
+        if area > 7 and area < 300 and ((h>7 and w<=5) or (h>5 and w>5 and area>15)):
             region = image_binn[y:y + h + 1, x:x + w + 1]
             regions_array.append([image_utils.resize_region(region), (x, y, w, h)])
             cv2.rectangle(image_orig, (x, y), (x + w, y + h), (255,0,0), 1)
 
-            filee.write("\n"+str(idx)+"\t"+str(x)+"\t"+str(y)+"\t"+str(w)+"\t"+str(h)+"\t"+str(area))
+            filee.write("\n"+str(idx)+"\t"+str(x)+"\t"+str(y)+"\t"+str(w)+"\t"+str(h)+"\t"+str(area)+"\t"+str(idz))
             idx = idx + 1
 
     filee.close()
